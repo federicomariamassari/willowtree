federicomariamassari/willowtree

commit dec5db55f1f0869bd0a08757d054682d9bdcb997

fix: Rule out probabilities outside range in LP
Add 0 < p < 1 constraint to linear programming problem

This commit adds two constraints to the linear programming problem
which automatically rule out probabilities outside range, p < 0 and
p > 1, so that all the generated matrices are now well-behaved.

Resolves: #4

python-modules/willowtree.py
@@ -205,7 +205,9 @@ def lp(z, q, t, tol = 1e-12, extra_precision = False):
         P = transition_matrix(z, c[i], Aeq, beq[i], tol,
                               extra_precision)
 
-        while (P.status != 0) | (P.fun < 0) | (P.fun > 1):
+        while (P.status != 0) | (P.fun < 0) | (P.fun > 1) \
+            | (P.x[P.x < 0]).any() | (P.x[P.x > 1]).any():
+
             if tol < 1e-3:
                 tol *= 10
                 P = transition_matrix(z, c[i], Aeq, beq[i], tol,

commit 8b769ec8c9017bc13575220bf7b42b2b60057962

fix: Refine constraint on probabilities in LP
Refine the constraint on probabilities to rule out p < 0 and
p > 1 while also accounting for occasional NaN output from the
linear programming algorithm.

The NaN output previously broke the algorithm, since the added
constraints '(P[P < 0]).any()' and '(P[P > 1]).any' raised the
TypeError: 'float' object is not subscriptable.

This commit fixes the error introduced in dec5db5 by checking
whether the output of the LP problem is float (NaN) and:

 - if not, by adding the two constraints on the LP problem;
 - otherwise, by automatically assigning flag -1 (failure) and
   moving on to the next problem.

However, the side effect is an increase in running time.
For this reason, the algorithm is now timed so that it stops
whenever any iteration lasts longer than one minute.

Resolves: #4

python-modules/willowtree.py
@@ -209,21 +209,24 @@ def lp(z, q, t, tol = 1e-12, extra_precision = False):
         P = transition_matrix(z, c[i], Aeq, beq[i], tol,
                               extra_precision)
 
-    if type(P.x) != np.float:
-        while (P.status != 0) | (P.fun < 0) | (P.fun > 1) \
-            | (P.x[P.x < 0]).any() | (P.x[P.x > 1]).any():
+        if type(P.x) != np.float:
+            start = time.time()
+            while (P.status != 0) | (P.fun < 0) | (P.fun > 1) \
+                | ((P.x[P.x < 0]).any()) | ((P.x[P.x > 1]).any()):
 
-            if tol < 1e-3:
-                tol *= 10
-                P = transition_matrix(z, c[i], Aeq, beq[i], tol,
-                                      extra_precision)
-            else:
-                flag[i] = -1
-                break
+                if (tol < 1e-3) & (time.time() - start < 60):
+                    tol *= 10
+                    P = transition_matrix(z, c[i], Aeq, beq[i], tol,
+                                          extra_precision)
+                else:
+                    flag[i] = -1
+                    break
 
-        Px[i] = P.x.reshape(len(z), len(z))
-    else:
-        flag[i] = -1
+            Px[i] = P.x.reshape(len(z), len(z))
+
+        else:
+            flag[i] = -1
+
 
         if flag[i] == -1:
             print('Warning: P[{}] wrongly specified.'.format(i))