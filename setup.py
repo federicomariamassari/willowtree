d2da7c190079b56158766590ccf34ab729385bd6
Federico Maria Massari <federico.massari@bocconialumni.it>
2017-10-28 08:21:07 +0200

    chore: Add docstrings to setup.py
    Add docstrings to setup.py and update the name of the package to be
    installed to 'willowtree' (from 'python-modules').

--- setup.py
+++ setup.py
@@ -1,15 +1,48 @@
 '''
-A setuptools bases setup module for the willow tree.
+A setuptools based setup module to install willowtree on your personal
+computer.
 
-Also see: https://github.com/pypa/sampleproject
+How does it work?
+-------------------------------------------------------------------------------
+On Terminal (macOS) or Command Prompt (Windows), navigate through folder
+'willow-tree', then run ($ is the Terminal prompt):
+
+$ python3 setup.py install
+
+This command will generate three folders in the current directory:
+
+build
+dist
+willowtree.egg-info
+
+and one .egg file in the main package folder, for example, if third-party
+extension Anaconda is installed, in 'anaconda/lib/python3.X/site-packages':
+
+willowtree-X.X-py3.X.egg
+
+where X.X refers to the most recent installed version of 'willowtree', e.g.
+0.9, and py3.X to the latest installed Python version, e.g. 3.6.
+
+It is now possible to import willowtree in a Python environment.
+
+References
+-------------------------------------------------------------------------------
+https://pythonhosted.org/an_example_pypi_project/setuptools.html
+https://github.com/pypa/sampleproject
 '''
 
 from setuptools import setup
 
-setup(
-    name='willow-tree',
+# Look for module __version__.py in willowtree and import variable __version__
+from willowtree.__version__ import __version__
 
-    version='0.9',
+# Assign the value of __version__ to variable version, then use it in setup()
+version = __version__
+
+setup(
+    name='willowtree',
+
+    version=version,
 
     description='''Robust and flexible Python implementation of the willow
     tree lattice for derivatives pricing.''',
@@ -35,10 +68,12 @@ setup(
     'standard-brownian-motion'
     ],
 
+    # The packages to install. There is only one folder, 'willowtree'
     packages=[
-    'python-modules'
+    'willowtree'
     ],
 
+    # Dependencies, the auxiliary libraries necessary to run 'willowtree'
     install_requires=[
     'numpy >= 1.13',
     'scipy >= 0.19',
