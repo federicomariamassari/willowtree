2ef02438149627ab1efc23b80b76f66fc1987871
Federico Maria Massari <federico.massari@bocconialumni.it>
2017-10-28 08:32:06 +0200

    feat: Add __version__.py to 'willowtree' folder
    Add __version__.py to the package. This module is both:
    
     - called by 'setup.py' during the installation phase, and
     - called by the user in Python with 'willowtree.__version__'.

---
+++ willowtree/__version__.py
@@ -0,0 +1,15 @@
+'''
+Return the version number of the currently installed 'willowtree' package.
+Version numbering follows a major.macro.micro convention, with micro omitted
+if equal to 0.
+
+To display version number in a Python environment, either call:
+
+willowtree.__version__
+
+or, if willowtree is imported as wt:
+
+wt.__version__
+'''
+
+__version__ = '0.9'
