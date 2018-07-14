1a4c37ed896d3213ed8e919737511bc61d355398
Federico Maria Massari <federico.massari@bocconialumni.it>
2018-07-14 16:52:02 +0200

    feat: Remove MATLAB version from repository

--- matlab-version/pricing_error.m
+++
@@ -1,9 +0,0 @@
-function [error_call,error_put] = pricing_error(call_BSM,call,put_BSM,put)
-
-% ERROR RELATIVE TO THE BLACK-SCHOLES-MERTON FUNCTION
-
-error_call = abs((call - call_BSM)/call_BSM);
-error_put = abs((put - put_BSM)/put_BSM);
-
-end
-
--- matlab-version/pricing_error_american.m
+++
@@ -1,8 +0,0 @@
-function [error_call_American,error_put_American] = pricing_error_american(call_BIN_American,call_American,put_BIN_American,put_American)
-
-% ERROR RELATIVE TO THE AMERICAN BINOMIAL FUNCTION
-
-error_call_American = abs((call_American - call_BIN_American)/call_BIN_American);
-error_put_American = abs((put_American - put_BIN_American)/put_BIN_American);
-
-end
